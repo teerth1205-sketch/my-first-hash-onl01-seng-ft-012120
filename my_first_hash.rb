2c469cf14d6e1568fc1a43d2bbd5dc689fb63482
Teerth Patel <teerth1205@gmail.com>
2020-01-23 22:06:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,5 @@
 def my_hash
+  your_hash = {"teerth" => "25", "weight" => "212lb"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
